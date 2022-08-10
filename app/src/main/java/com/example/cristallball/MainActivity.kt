c767b63483743c110dd962fb3dac9208a4753e0e
EvgeniyTLT <Toy444@ya.ru>
2022-08-10 23:19:34 +0300

    Initial commit

---
+++ app/src/main/java/com/example/cristallball/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cristallball
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
